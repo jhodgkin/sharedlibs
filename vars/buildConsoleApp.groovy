2fbcb51a5b7d2b43d4491f04fe87fb1ffcdc2033
Jerod Hodgkin <jhodgkin@conservice.com>
2021-11-19 16:12:15 -0700

    Add buildConsoleApp.groovy

---
+++ vars/buildConsoleApp.groovy
@@ -0,0 +1,105 @@
+
+
+def call(body){
+	def params = [:]
+	body.resolveStrategy = Closure.DELEGATE_FIRST
+	body.delegate = params
+	body()
+    if (!params.containsKey("UnitTests")) {
+        params.put("UnitTests", []);
+    }
+
+    if (!params.containsKey("IntegrationTests")) {
+        params.put("IntegrationTests", []);
+    }
+
+    if (!params.containsKey("RegressionTests")) {
+        params.put("RegressionTests", []);
+    }
+
+    // GLOBAL CONSTANTS
+    def Deployed = false
+    def TagName = ""
+    def TestCategoryFilter = "TestCategory=All"
+    def BranchName = ""
+
+    // GLOBALS: MSBUILD PARAMS
+    def BuildConfig = "Debug"
+    def SolutionBuildPlatform = "\"Any CPU\""
+    def ProjectBuildPlatform = "\"AnyCPU\""
+    def SolutionBuildString = "/p:Configuration=${BuildConfig} /p:Platform=${SolutionBuildPlatform}"
+    def ProjectBuildString = "/p:Configuration=${BuildConfig} /p:Platform=${ProjectBuildPlatform}"
+
+    // GLOBALS: PARTIAL PATHS
+    def MainProjectFolder = ""
+
+    // GLOBALS: PATHS
+    def MainSolutionPath = ""
+    def MainProjectPath = ""
+    def versionNumber = ""
+
+    pipeline {
+        agent any
+
+        stages {
+            stage ('Init') {
+                steps {
+                    script {
+                        BranchName = env.BRANCH_NAME
+
+                        if (BranchName == "master" || BranchName == "main") {
+                            BuildConfig = "Release"
+                            SolutionBuildString = "/p:Configuration=${BuildConfig} /p:Platform=${SolutionBuildPlatform}"
+                            ProjectBuildString = "/p:Configuration=${BuildConfig} /p:Platform=${ProjectBuildPlatform}"
+                        }
+
+                        // on any non-master build, we also want to run automated tests in the "Dev" category
+                        if (BranchName != "master" && BranchName != "main") {
+                            TestCategoryFilter = TestCategoryFilter + "|TestCategory=Dev"
+                        }
+
+                        // build project paths
+                        MainProjectFolder = "${env.WORKSPACE}\\${params.MainProjectName}"
+                        if (params.containsKey("MainProjectFolder")) {
+                            MainProjectFolder = params.MainProjectFolder
+                        }
+                        
+                        MainSolutionPath = "\"${env.WORKSPACE}\\${params.SolutionName}.sln\""
+                        if (params.containsKey("MainSolutionPath")) {
+                            MainSolutionPath = params.MainSolutionPath
+                        }
+                        
+                        MainProjectPath = "\"${MainProjectFolder}\\${params.MainProjectName}.csproj\""
+                        if (params.containsKey("MainProjectPath")) {
+                            MainProjectPath = params.MainProjectPath
+                        }
+
+                        versionNumberOutput = bat (
+                            script: "${env.BuildScriptsFolder}AssemblyVersionGet.bat ${MainProjectFolder}",
+                            returnStdout: true
+                        ).trim()
+
+                        try{
+                            versionNumber = versionNumberOutput.split()[-1]
+                        }
+                        catch(Exception ex){
+                            
+                        }
+                        echo "Version Number: ${versionNumber}"
+                    }
+
+                    echo "BranchName: ${BranchName}"
+                    echo "BuildConfig: ${BuildConfig}"
+                    echo "MainProjectName: ${params.MainProjectName}"
+
+                    // prints all environment variables, for debugging purposes
+                    // bat "set"
+                }
+            }
+    	}
+
+    	post {
+    		echo "We DID IT!"
+    	}
+    }
+}
