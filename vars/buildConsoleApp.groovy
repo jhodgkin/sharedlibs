5ab63a67147745850dadd31a9ff0eb8dfed789ed
Jerod Hodgkin <jhodgkin@conservice.com>
2021-11-23 10:23:01 -0700

    main == master

--- vars/buildConsoleApp.groovy
+++ vars/buildConsoleApp.groovy
@@ -235,7 +235,7 @@ def call(body){
 
             stage('Deploy'){
                 when {
-                    expression { BRANCH_NAME ==~ /master|main/ }
+                    expression { branch ==~ /master|main/ }
                 }
                 steps{
                     script {
