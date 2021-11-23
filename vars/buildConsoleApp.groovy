f32527e34d2d2198d33ebb748566c7c1261376f7
Jerod Hodgkin <jhodgkin@conservice.com>
2021-11-23 11:14:05 -0700

    printenv

--- vars/buildConsoleApp.groovy
+++ vars/buildConsoleApp.groovy
@@ -81,10 +81,7 @@ def call(body){
 			stage("Env Variables") {
 				steps {
 					script {
-						env.LS = sh(script:'ls -lah', returnStdout: true).trim()
-						echo "LS = ${env.LS}"
-						// or if you access env variable in the shell command
-						sh 'echo $LS'
+						bat "set"
 					}
 				}
 			}
